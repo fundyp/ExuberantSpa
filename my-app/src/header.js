3078a69c2370043192ca627a2ba32c67712047a2
fundyp <144487692+fundyp@users.noreply.github.com>
2024-08-31 01:36:43 -0400

    dynamic nav bar
    making navbar more dynamic with size changes, scrolling, and hover options

--- my-app/src/header.js
+++ my-app/src/header.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import './header.css';  // Import the corresponding CSS file
-//import exuberantLogo from './public/exuberantlogo.png';
 
 function Header() {
   return (
     <header className="header">
       <img src={`${process.env.PUBLIC_URL}/exuberantlogo.png`} alt="Exuberant Beauty Spa" className="logo" />
       <div className="header-divider"></div>
-      <nav>
+      <nav className="nav-container">
+        <h2 className="nav-header">Exuberant</h2>
         <ul className="nav-list">
           <li className="nav-item">Home</li>
           <li className="nav-item">About Us</li>
           <li className="nav-item">Services</li>
           <li className="nav-item">Gallery</li>
-          <li className="nav-item">Contact</li>
+          <li className="nav-item">Contact Us</li>
           <li className="nav-item">Reviews</li>
-
         </ul>
       </nav>
       <div className="header-divider"></div>
